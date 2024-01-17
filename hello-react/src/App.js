adb1af0f567354272ff7b408d338493a43bc1ee9
HUNMINRYU <ryu456321@naver.com>
2024-01-17 15:04:17 +0900

    닫는태그 필요함

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return <div className="react"> {name} </div>; 
+  return (
+    <>
+      <div className="react"> {name} </div>
+      <input></input>
+    </>
+  ); 
 }
+// 닫는태그가 반드시 있어야함
 export default App;
