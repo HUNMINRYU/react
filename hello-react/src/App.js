a04d8f7d1cba9540c13ca736fab15c88a182ef47
HUNMINRYU <ryu456321@naver.com>
2024-01-17 13:54:43 +0900

    표현식

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,11 @@
-import { Fragment } from 'react';
-
 function App() {
+  const name = "리액트";
+  //const : 안변함 let: 변함
   return (
-    <>     
-      <h1>리액트 안녕!</h1>
+    <>
+      <h1>{name} 안녕!</h1>
       <h2>잘 작동하니?</h2>
     </>
   );
 }
-// <Fragment></Fragment>
 export default App;
