cbb82d056422602e5ec8423dd7f50cad4ed57546
HUNMINRYU <ryu456321@naver.com>
2024-01-24 12:05:34 +0900

    5.2.3.1 input에 ref달기

--- hello-react/src/ValidationSample.js
+++ hello-react/src/ValidationSample.js
@@ -27,7 +27,7 @@ class ValidationSample extends Component {
     return (
       <div>
         <input
-          //ref={this.input}
+          ref={(ref) => (this.input = ref)}
           type="password"
           value={this.state.password}
           onChange={this.handleChange}
